b2f41fada262288d596240282b66c1bbef9cd9fd
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-03-23 12:49:50 -0400

    Added the Html file that shows a table in CSS

--- StudentManagementSpringBootTymeleaf/src/main/java/org/JoshuaGuzman/SpringBootStudent/controller/StudentController.java
+++ StudentManagementSpringBootTymeleaf/src/main/java/org/JoshuaGuzman/SpringBootStudent/controller/StudentController.java
@@ -1,8 +1,23 @@
 package org.JoshuaGuzman.SpringBootStudent.controller;
 
+import org.JoshuaGuzman.SpringBootStudent.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    //handler method for list of students and return the view
+    @GetMapping("/students")
+    public  String listStudents(Model model){
+    model.addAttribute("students", studentService.getAllStudents());
+        return "students";
+    }
 
 }
