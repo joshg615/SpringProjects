00edc790f2bd124799c39bc39af30b8f7c87cdb7
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-03-23 09:25:38 -0400

    Started on Student Management
    this project will be using Spring Boot and Tymeleaf

---
+++ StudentManagementSpringBootTymeleaf/src/main/java/org/JoshuaGuzman/SpringBootStudent/App.java
@@ -0,0 +1,13 @@
+package org.JoshuaGuzman.SpringBootStudent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
