a9dceb929cf23a004543acb30f0c9ad10166a95a
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-03-23 11:37:29 -0400

    Added javax persistence dependency
    Added javax persistence annotations to student class entity

--- StudentManagementSpringBootTymeleaf/src/main/java/org/JoshuaGuzman/SpringBootStudent/entity/Student.java
+++ StudentManagementSpringBootTymeleaf/src/main/java/org/JoshuaGuzman/SpringBootStudent/entity/Student.java
@@ -1,13 +1,26 @@
 package org.JoshuaGuzman.SpringBootStudent.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="students" )
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
 
     public Student(){
-        
+
     }
     public Student( String firstName, String lastName, String email) {
         this.firstName = firstName;
